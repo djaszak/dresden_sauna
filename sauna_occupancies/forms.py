17f02a21c52d285d07134fc7d916399d33a25741
dennis.jaszak <dennis.jaszak@descript.de>
2019-01-08 20:12:18 +0100

    Add check if there is already an occuoancy when someone wants to start

--- sauna_occupancies/forms.py
+++ sauna_occupancies/forms.py
@@ -17,6 +17,16 @@ class UltraTimeInput(TimeInput):
     input_type = 'time'
 
 
+# Credits to
+# https://stackoverflow.com/questions/10048249/how-do-i-determine-if-current-time-is-within-a-specified-range-using-pythons-da
+def is_time_in_time_period(start_time, end_time, check_time):
+    if start_time < end_time:
+        return check_time >= start_time and check_time <= end_time
+    # Over midnight
+    else:
+        return check_time >= start_time or check_time <= end_time
+
+
 class OccupancyForm(forms.ModelForm):
     occupancy_date = forms.DateField()
     start_time = forms.TimeField()
@@ -55,18 +65,37 @@ class OccupancyForm(forms.ModelForm):
         start_time = cleaned_data.get('start_time')
         end_time = cleaned_data.get('end_time')
         now = datetime.datetime.now()
+        specific_day_occupancies = Occupancy.objects.filter(
+            start__year=occupancy_date.year,
+            start__month=occupancy_date.month,
+            start__day=occupancy_date.day
+        )
 
         if start_time:
             if now.day > occupancy_date.day:
                 raise forms.ValidationError(
                     'Du kannst doch nicht in der Vergangenheit saunieren, Dummkopf :)'
                 )
+            for occupancy in specific_day_occupancies:
+                occupancy_start_time = datetime.time(
+                    hour=occupancy.start.hour,
+                    minute=occupancy.start.minute
+                )
+                occupancy_end_time = datetime.time(
+                    hour=occupancy.end.hour,
+                    minute=occupancy.end.minute
+                )
+                if is_time_in_time_period(occupancy_start_time, occupancy_end_time, start_time):
+                    raise forms.ValidationError(
+                        'Es wird bereits sauniert, wenn du starten möchtest, schade :('
+                    )
 
         if start_time and end_time:
             if end_time < start_time:
                 raise forms.ValidationError(
                     'Du kannst das saunieren nicht beenden bevor du gestartet hast'
                 )
+
 
     def save(self, commit=True):
         model = super(OccupancyForm, self).save(commit=False)
