8f77d089f7e422cf6bda5ed73db9f8dd0ff0e2dd
dennis.jaszak <dennis.jaszak@descript.de>
2019-01-08 14:31:11 +0100

    Csv file mechanism added, prefilling, saving, cleaning of occupancy form works, index occupancy calendar works, with links to occupancy lists and create_occupancy

--- sauna_occupancies/views.py
+++ sauna_occupancies/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import calendar
+import csv
+import datetime
+import os
+
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render
@@ -12,9 +17,20 @@ from .models import Occupancy
 
 @login_required(login_url='/sauna_occupancies/login/')
 def index(request):
-    occupancies = Occupancy.objects.all()[:5]
-
-    return render(request, 'index.html', {'occupancies': occupancies})
+    now = datetime.datetime.now()
+    days_of_month = []
+    monthrange = calendar.monthrange(now.year, now.month)
+    for x in range(monthrange[0]):
+        days_of_month.append(('', False))
+    for y in range(monthrange[1]):
+        if Occupancy.objects.filter(start__year=now.year, start__month=now.month, start__day=y+1).exists():
+            days_of_month.append((y + 1, True))
+        else:
+            days_of_month.append((y + 1, False))
+    return render(request, 'index.html', {'days_of_month': days_of_month,
+                                          'year': now.year,
+                                          'month': calendar.month_name[now.month],
+                                          'month_number': now.month})
 
 
 @login_required(login_url='/sauna_occupancies/login/')
@@ -25,10 +41,59 @@ def create_occupancy(request):
             occupancy = form.save()
             occupancy.user.add(request.user)
             occupancy.save()
-            return redirect(index)
 
+            # Necassary to get the actual path to our CSV File
+            workpath = os.path.dirname(os.path.abspath(__file__))
+            ccron = os.path.join(workpath, 'ccron.csv')
+
+            # Mechanism to get latest cronjobID, if there is none yet
+            # we get IndexError and generate the least possible ID
+            with open(ccron, newline='') as CSVFile:
+                reader = list(csv.reader(CSVFile))
+                try:
+                    # We have to get the last row, the ID is the 9th cell and then we need the numeral ID
+                    # without CJ at the beginning
+                    numeral_id = int(reader[-1][8][2:])
+                    # Now we increment the ID and generate our new cronjobID
+                    numeral_id += 1
+                    cronjob_id = 'CJ' + str(numeral_id)
+                except IndexError:
+                    cronjob_id = 'CJ0'
+
+            # Now we will append a new row to the cronjob.csv
+            with open(ccron, 'a') as csvFile:
+                writer = csv.writer(csvFile)
+                # [sec],[min],[hour],[day],*,[month],[year],1,[ID],[comment],0,24,1,*
+                minute = occupancy.start.strftime('%M')
+                hour = occupancy.start.strftime('%H')
+                day = occupancy.start.strftime('%d')
+                month = occupancy.start.strftime('%m')
+                year = occupancy.start.strftime('%Y')
+                comment = occupancy.user.all()[0].__str__()
+                writer.writerow([
+                    '00',
+                    minute,
+                    hour,
+                    day,
+                    '*',
+                    month,
+                    year,
+                    '1',
+                    cronjob_id,
+                    comment,
+                    '0',
+                    '24',
+                    '1',
+                    '*'
+                ])
+            return redirect(index)
     else:
         form = OccupancyForm()
+        for key in request.GET:
+            try:
+                form.fields[key].initial = request.GET[key]
+            except KeyError:
+                pass
 
     return render(request, 'create_occupancy.html', {'form': form})
 
@@ -58,4 +123,10 @@ def add_user_to_occupancy(request, occupancy_id):
         occupancy.save()
         messages.success(request, 'Nice, du saunierst dann auch mit')
         return redirect(change_occupancy, occupancy_id=occupancy.id)
+
+
+@login_required(login_url='/sauna_occupancies/login/')
+def occupancy_list(request, year, month, day):
+    occupancies = Occupancy.objects.filter(start__year=year, start__month=month, start__day=day)
+    return render(request, 'occupancy_list.html', {'occupancies': occupancies})
 
