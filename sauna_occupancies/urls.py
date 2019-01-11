c3468ac739b103d0cacaa375e60fc6b2c23e73c9
dennis.jaszak <dennis.jaszak@descript.de>
2019-01-11 19:26:55 +0100

    Add arrows to navigate through the months in occupancy_calendar

--- sauna_occupancies/urls.py
+++ sauna_occupancies/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.occupancy_calendar, name='occupancy_calendar'),
     path('occupancy/create-occupancy/', views.create_occupancy, name='create_occupancy'),
     path('occupancy/change/<int:occupancy_id>/', views.change_occupancy, name='change_occupancy'),
     path('occupancy/change/<int:occupancy_id>/add_user/', views.add_user_to_occupancy, name='add_user_to_occupancy'),
     path('occupancy/list/<int:year>/<int:month>/<int:day>/', views.occupancy_list, name='occupancy_list'),
-    path('login/', auth_views.LoginView.as_view(redirect_field_name='index'), name='login'),
+    path('login/', auth_views.LoginView.as_view(redirect_field_name='occupancy_calendar'), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('csv-download/', views.csv_download, name='csv_download')
 ]
---
+++ sauna_occupancies/view_utils.py
@@ -0,0 +1,7 @@
+# Found: https://stackoverflow.com/questions/3424899/whats-the-simplest-way-to-subtract-a-month-from-a-date-in-python
+def monthdelta(date, delta):
+    m, y = (date.month+delta) % 12, date.year + ((date.month)+delta-1) // 12
+    if not m: m = 12
+    d = min(date.day, [31,
+        29 if y%4==0 and not y%400==0 else 28,31,30,31,30,31,31,30,31,30,31][m-1])
+    return date.replace(day=d,month=m, year=y)
